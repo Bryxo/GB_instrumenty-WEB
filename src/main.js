692ad0be5440b94a868fa30ef62ae9f41ae03b9a
Bryxo <bryxo.job@yandex.ru>
2022-12-16 18:03:58 +0300

    homework1

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import { diffDates, diffToHtml } from "./datecalc.js";
+import { formatError } from "./utils.js";
+import { showHideTimeCalc, showHideDateCalc } from "./showHide.js";
+//import { buttonRun }  from "./timer.js";
+//import { Howl, Howler } from "./howler.js";
+// import { Howl, Howler } from 'howler';
+
+const dateCalcForm = document.getElementById("datecalc"); 
+const dateCalcResult = document.getElementById("datecalc__result"); 
+
+dateCalcForm.addEventListener("submit", handleCalcDates); 
+
+function handleCalcDates(event) { 
+    dateCalcResult.innerHTML = ""; 
+    event.preventDefault(); 
+
+    let { firstDate, secondDate } = event.target.elements; 
+    firstDate = firstDate.value, secondDate = secondDate.value; 
+    
+    if (firstDate && secondDate) { 
+        const diff = diffDates(firstDate, secondDate);
+        dateCalcResult.innerHTML = diffToHtml(diff);
+    }
+    else dateCalcResult.innerHTML = formatError("Для расчета промежутка необходимо заполнить оба поля с датами");
+}
+
+
